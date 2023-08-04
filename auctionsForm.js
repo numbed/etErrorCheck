b60412191e8cb11efd30424c9d3bc2d12ac53fc2
numbed <rashkov.nikola@gmail.com>
2023-08-04 14:17:00 +0300

    need changes so for older auctions openOrder name

--- auctionsForm.js
+++ auctionsForm.js
@@ -1,6 +1,6 @@
 console.log("auctionForm");
 cancelOrderCheck();
-// auctionSave();
+auctionSave();
 
 
 if (commDateCheck() === true) {
@@ -22,7 +22,7 @@ if (documentsSelectFields.length != 0 & firstByuerDocs.length != 0) {
 //removes the need to populate TITLE and DESCRIPTION input fields before saving new auction
 function auctionSave() {
     console.log("auctionSave()");
-    if (document.querySelector('#auctionTitle').value === "" || document.querySelector('#auctionDescription').value === "") {
+    if ((document.querySelector('#auctionTitle').value === "" || document.querySelector('#auctionDescription').value === "") & firstByuerDocs.length === 0) {
         let tt = document.querySelectorAll(".form-group.has-feedback button");
         tt.forEach(el => {
             el.click();
