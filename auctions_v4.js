e04313dbec246c3f6ab56fb4950ee9da96c871f8
numbed <rashkov.nikola@gmail.com>
2023-09-19 15:49:54 +0300

    ...

--- auctions_v4.js
+++ auctions_v4.js
@@ -43,16 +43,33 @@ let infoTable = [{
         id: 'commission',
         title: 'commission',
         color: '#040D12'
-    },
+    }
 ];
 
 function main() {
     createInfoTable();
     createButton();
 
+    // setAuctionsClasses() must be before showDeadline() or it will not work properly !!! TO BE FIXED !!!
+    setAuctionsClasses();
+    showDeadline();
+
 }
 main();
 
+// show deadline for publishing documents in forth column
+// called in main()
+function showDeadline() {
+    auctionsTable.forEach(el => {
+        let dateToShow = deadlineCheck(el.cells[2].innerText).getDate() + "." + (deadlineCheck(el.cells[2].innerText).getMonth() + 1) + "." + deadlineCheck(el.cells[2].innerText).getFullYear();
+        console.log("🚀 ~ file: auctions_v4.js:65 ~ showDeadline ~ deadlineCheck(el.cells[2].innerText).getMonth(): LOADED", deadlineCheck(el.cells[2].innerText).getMonth())
+        if (!el.cells[3].innerHTML.includes('br')) {
+            el.cells[3].innerHTML += '<br>' + dateToShow.italics().bold();
+        }
+    });
+}
+
+// called in main()
 function createInfoTable() {
     // separate left side navgiation menu from newly created info table with horizontal line
     let hr = document.createElement('hr');
@@ -86,6 +103,7 @@ function createInfoTable() {
     }
 }
 
+// called in main()
 function createButton() {
     // Create container element
     const div = document.createElement('div');
@@ -106,33 +124,123 @@ function createButton() {
     div.appendChild(button);
 }
 
+// called in createButton()
 function buttonClick() {
     alert('Button clicked!');
 }
 
+// called in showDeadline() | auctionDateCheck()
+function deadlineCheck(date) {
+    let firstDate = stringToDate(date);
+    console.log("🚀 ~ file: auctions_v4.js:135 ~ deadlineCheck ~ firstDate: LOADED", firstDate)
+    let deadlineDate = new Date();
+    let deadline = new Date();
+
+    if (firstDate.getDay() == 1 || firstDate.getDay() == 4) {
+        deadline = firstDate.getDate() - 20;
+    } else if (firstDate.getDay() == 2 || firstDate.getDay() == 5) {
+        deadline = firstDate.getDate() - 18;
+    } else if (firstDate.getDay() == 3) {
+        deadline = firstDate.getDate() - 19;
+    }
+    deadlineDate.setDate(deadline);
+    return deadlineDate;
+}
+
+// called in auctionDateCheck()
+function commissionCheck(date) {
+    let firstDate = stringToDate(date);
+    let commissionDate = new Date();
+    let commission = new Date();
+
+    if (firstDate.getDay() == 1) {
+        commission = firstDate.getDate() - 3;
+    } else {
+        commission = firstDate.getDate() - 1;
+    }
+    commissionDate.setDate(commission);
+    return commissionDate;
+}
+
+// called in main()
+function setAuctionsClasses() {
+    auctionsTable.forEach(el => {
+        if (window.getComputedStyle(el).color === "rgb(153, 153, 153)") {
+            el.cells[(el.querySelectorAll('td').length - 1)].className = 'notPublished';
+        } else {
+            el.cells[(el.querySelectorAll('td').length - 1)].className = auctionDateCheck(el);
+        }
+    })
+}
+
+// called in setAuctionsClasses()
+function auctionDateCheck(el) {
+    let string = el.cells[2].innerText
+    let deadline = deadlineCheck(string);
+    let commission = commissionCheck(string);
+    
+
+    if (deadline.setHours(0, 0, 0, 0) == today.setHours(0, 0, 0, 0)) {
+        return "today";
+    } else if (deadline.setHours(0, 0, 0, 0) < today.setHours(0, 0, 0, 0) && commission.setHours(0, 0, 0, 0) == today.setHours(0, 0, 0, 0)) {
+        return "commission";
+    } else if (deadline.setHours(0, 0, 0, 0) < today.setHours(0, 0, 0, 0)) {
+        return "passed";
+    } else {
+        return "future";
+    }
+}
+
+// called in stringToDate()
+function stringToDate(string) {
+    string = string.split(" ")[0].split(".");
+    return new Date(string[2], (string[1] -1 ), string[0]);
+}
+
 //styling bellow
 document.head.insertAdjacentHTML("beforeend", `<style>
-#infoTable {
-    padding: 14px 20px 14px 25px;
-    color: #a7b1c2;
-    font-weight: 600;
-}
 
-#infoTable td {
-    padding: 10px;
-    border-bottom: 1px solid;
-}
+    .passed {
+        background-color: #81B622;
+    }
 
-#infoTable tr:hover {
-    background-color: #293846;
-    color: white;
-}
+    .future {
+        background-color: #FFBB5C;
+    }
 
-#infoButtonContainer {
-    padding: 10px;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    height: 30px;
-}
-    </style>`);
+    .today {
+        background-color: #D1462F;
+    }
+
+    .commission {
+        background-color: #040D12;
+    }
+
+    .notPublished {
+        background-color: rgb(153, 153, 153);
+    }
+
+    #infoTable {
+        padding: 14px 20px 14px 25px;
+        color: #a7b1c2;
+        font-weight: 600;
+    }
+
+    #infoTable td {
+        padding: 10px;
+        border-bottom: 1px solid;
+    }
+
+    #infoTable tr:hover {
+        background-color: #293846;
+        color: white;
+    }
+
+    #infoButtonContainer {
+        padding: 10px;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        height: 30px;
+    }
+        </style>`);
