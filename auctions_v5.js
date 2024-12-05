e6d7bf6035f6e42b7afca09643616ddd5fc8be08
numbed <rashkov.nikola@gmail.com>
2024-12-05 11:33:13 +0200

    added pills for woodsInfo and auctionDetails (opacity fix needed)

--- auctions_v5.js
+++ auctions_v5.js
@@ -75,8 +75,7 @@ function adjustWorkingDays(date, days) {
           doc.querySelector('[name="data[woodInfo][firewood][0]"]')?.value ||
           null,
         ozm: doc.querySelector('[name="data[woodInfo][ozm][0]"]')?.value || null,
-        small:
-          doc.querySelector('[name="data[woodInfo][small][0]"]')?.value || null,
+        small: doc.querySelector('[name="data[woodInfo][small][0]"]')?.value || null,
         mid: doc.querySelector('[name="data[woodInfo][mid][0]"]')?.value || null,
         big: doc.querySelector('[name="data[woodInfo][big][0]"]')?.value || null,
       };
@@ -124,61 +123,138 @@ function adjustWorkingDays(date, days) {
     }
   }
   
-  // Update the table with column7 data
-  function updateTable(tableData) {
+  // Update the table with column6 and column7 data
+function updateTable(tableData) {
     tableData.forEach((rowData) => {
-      const { rowElement, column7, startPrice } = rowData;
-      const cells = rowElement.querySelectorAll("td");
-  
-      if (cells[6] && column7) {
-        // 7th cell is at index 6
-        // Calculate 5% of the start price
-        const maxGuarantee = column7.startPrice * 0.05;
-  
-        // Highlight guarantee if it exceeds 5% of the start price
-        const guaranteeStyle =
-          column7.auctionGuarantee > maxGuarantee
-            ? "color: red; font-weight: bold;"
-            : "";
-        const guaranteePercentage =
-          (column7.auctionGuarantee / column7.startPrice.toFixed(2)) * 100;
-  
-        cells[6].innerHTML = `
-                  <div><strong>Start price:</strong> ${
-                      column7.startPrice || "N/A"
-                    }</div>
-                  <div><strong>Bid Step:</strong> ${
-                    column7.auctionBidStep || "N/A"
-                  }</div>
-                  <div><strong>Guarantee:</strong> 
-                      <span style="${guaranteeStyle}">${
-          column7.auctionGuarantee || "N/A"
+        const { rowElement, column6, column7, startPrice } = rowData;
+        const cells = rowElement.querySelectorAll("td");
+
+        // Update the 6th cell with column6 data
+        // if (cells[5] && column6) {
+        //     cells[5].innerHTML += `
+        //     <div id='woodsInfo'>
+        //         <span title='Big'> ${column6.big || "N/A"}</span>
+        //         <span title='Mid'> ${column6.mid || "N/A"}</span>
+        //         <span title='Small'> ${column6.small || "N/A"}</span>
+        //         <span title='OZM'> ${column6.ozm || "N/A"}</span>
+        //         <span title='Firewood'> ${column6.firewood || "N/A"}</span>
+        //         <span title='Total'> ${column6.total || "N/A"}</span>
+        //     </div>
+        //     `;
+        // }
+        if (cells[5] && column6) {
+            cells[5].innerHTML += `
+                <div id='woodsInfo'>
+                    <span title='Big' style="opacity: ${column6.big === 0 ? 0.3 : 0.7};">
+                        ${column6.big !== undefined && column6.big !== null ? column6.big : "N/A"}
+                    </span>
+                    <span title='Mid' style="opacity: ${column6.mid === 0 ? 0.3 : 0.7};">
+                        ${column6.mid !== undefined && column6.mid !== null ? column6.mid : "N/A"}
+                    </span>
+                    <span title='Small' style="opacity: ${column6.small === 0 ? 0.3 : 0.7};">
+                        ${column6.small !== undefined && column6.small !== null ? column6.small : "N/A"}
+                    </span>
+                    <span title='OZM' style="opacity: ${column6.ozm === 0 ? 0.3 : 0.7};">
+                        ${column6.ozm !== undefined && column6.ozm !== null ? column6.ozm : "N/A"}
+                    </span>
+                    <span title='Firewood' style="opacity: ${column6.firewood === 0 ? 0.3 : 0.7};">
+                        ${column6.firewood !== undefined && column6.firewood !== null ? column6.firewood : "N/A"}
+                    </span>
+                    <span title='Total' style="opacity: ${column6.total === 0 ? 0.3 : 0.7};">
+                        ${column6.total !== undefined && column6.total !== null ? column6.total : "N/A"}
+                    </span>
+                </div>
+            `;
         }
-                      (${guaranteePercentage.toFixed(3)}%)</span>
-                  </div>
-              `;
-      }
+        
+
+
+        // Update the 7th cell with column7 data
+        if (cells[6] && column7) {
+            const maxGuarantee = column7.startPrice * 0.05;
+        
+            // Highlight guarantee and percentage if it exceeds 5% of the start price
+            const guaranteeStyle =
+                column7.auctionGuarantee > maxGuarantee
+                    ? "background-color: red; font-weight: bold; display: inline-block;"
+                    : "";
+        
+            const guaranteePercentage =
+                column7.auctionGuarantee && column7.startPrice
+                    ? ((column7.auctionGuarantee / column7.startPrice) * 100).toFixed(2)
+                    : "N/A";
+        
+            cells[6].innerHTML = `
+                <div><strong>Start price: ${column7.startPrice || "N/A"}</strong></div>
+                <div id='auctionDetails'>
+                    <span title='Bid Step'> ${column7.auctionBidStep || "N/A"}</span>
+                    <span title='Guarantee' style="${guaranteeStyle}">${column7.auctionGuarantee || "N/A"}</span>
+                    <span title='Percentage' style="${guaranteeStyle}">${guaranteePercentage} %</span>
+                </div>
+            `;
+        }
     });
-  }
-  
-  // Main function
-  async function processTable() {
+}
+
+// Main function
+async function processTable() {
     const tableData = extractTableData();
-  
+
     for (const row of tableData) {
-      if (row.link) {
-        const additionalData = await fetchAdditionalData(row.link);
-        row.column5 = additionalData.column5;
-        row.column6 = additionalData.column6;
-        row.column7 = additionalData.column7;
-        row.startPrice = additionalData.startPrice;
-      }
+        if (row.link) {
+            const additionalData = await fetchAdditionalData(row.link);
+            row.column5 = additionalData.column5;
+            row.column6 = additionalData.column6;
+            row.column7 = additionalData.column7;
+            row.startPrice = additionalData.column7.startPrice;
+        }
     }
-  
+
     updateTable(tableData);
     console.log("Processed Data:", tableData);
     return tableData;
-  }
-  
-  // Run the script
-  processTable();  
+}
+
+// Run the script
+processTable();
+
+
+//styling bellow
+document.head.insertAdjacentHTML(
+    "beforeend",
+    `<style>
+
+/* woodsinfo number of documents styling*/
+#docsLengthInfo {
+    display: flex;
+    width: 200px;
+}
+#woodsInfo,
+#auctionDetails{
+    display: flex;
+    width: auto;
+}
+#offers-pills {
+    display: flex;
+    width: 90px;
+}
+
+#docsLengthInfo>span,
+#woodsInfo>span,
+#auctionDetails>span,
+#offers-pills>span {
+    flex: 1;
+    padding-left: 2px;
+    padding-right: 2px;
+    margin-right: 5px;
+    width: 30%;
+    border-radius: 6px;
+    background-color: grey;
+    opacity: 0.7;
+    text-align: center;
+    font-size: small;
+    font-weight: 550;
+    color: white;
+}
+</style>`
+);
